4b72f488c2aafccce901540834751c9deabce661
linsicai <linsicai@gmail.com>
2018-07-28 23:42:49 +0800

    remove for code learning

--- scrapy/extension.py
+++ scrapy/extension.py
@@ -3,11 +3,13 @@ The Extension Manager
 
 See documentation in docs/topics/extensions.rst
 """
+
 from scrapy.middleware import MiddlewareManager
+
 from scrapy.utils.conf import build_component_list
 
+# 扩展中间件
 class ExtensionManager(MiddlewareManager):
-
     component_name = 'extension'
 
     @classmethod
--- scrapy/signals.py
+++ scrapy/signals.py
@@ -5,16 +5,22 @@ These signals are documented in docs/topics/signals.rst. Please don't add new
 signals here without documenting them there.
 """
 
+# 信号集合
+
 engine_started = object()
 engine_stopped = object()
+
 spider_opened = object()
 spider_idle = object()
 spider_closed = object()
 spider_error = object()
+
 request_scheduled = object()
 request_dropped = object()
+
 response_received = object()
 response_downloaded = object()
+
 item_scraped = object()
 item_dropped = object()
 
